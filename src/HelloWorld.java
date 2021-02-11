89b0b4d2ff5529f2aff0e8bd23cf409d3cd1c4d5
JHandy24 <jmhandy@quinnipiac.edu>
2021-02-11 13:42:57 -0500

    Created two Jack objects in Hello World class. One is present Jack and one is older Jack.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World! My name is Jack Handy");
+		Jack jack1 = new Jack(19, "Brown");
+		Jack oldJack = new Jack(91, "Grey");
 	}
 
 }
