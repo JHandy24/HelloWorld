7001450833ad9509669b6f48c4b8ec576fd150b1
JHandy24 <jmhandy@quinnipiac.edu>
2021-02-11 13:54:00 -0500

    Created a new Jack object called youngJack and set it to 5 years old and brown hair

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,9 @@ public class HelloWorld {
 		System.out.println("Hello World! My name is Jack Handy");
 		Jack jack1 = new Jack(19, "Brown");
 		Jack oldJack = new Jack(91, "Grey");
+		Jack youngJack = new Jack(5, "Brown");
 		System.out.println("Wow Jack. Nice to meet you. You are getting old!");
+		
 	}
 
 }
