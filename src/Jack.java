59a17f7e562fb2da267846869858a82f372799bc
JHandy24 <jmhandy@quinnipiac.edu>
2021-02-11 13:37:29 -0500

    Creating a class Jack with age and hair color

---
+++ src/Jack.java
@@ -0,0 +1,28 @@
+public class Jack {
+
+	private int age;
+	private String hairColor;
+
+public Jack(int age, String hairColor) {
+	this.age = age;
+	this.hairColor = hairColor;
+}
+
+public int getAge() {
+	return age;
+}
+
+public void setAge() {
+	this.age = age;
+}
+
+public String getHairColor() {
+	return hairColor;
+}
+
+public void setHairColor() {
+	this.hairColor = hairColor;
+	
+}
+
+}
